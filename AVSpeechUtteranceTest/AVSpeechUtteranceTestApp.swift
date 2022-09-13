63d136661e95f51b6f8b563c878ce98172c97c43
Dong487 <Dsheng7777777@gmail.com>
2022-09-14 02:26:55 +0800

    Initial Commit

---
+++ AVSpeechUtteranceTest/AVSpeechUtteranceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVSpeechUtteranceTestApp.swift
+//  AVSpeechUtteranceTest
+//
+//  Created by DONG SHENG on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct AVSpeechUtteranceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
